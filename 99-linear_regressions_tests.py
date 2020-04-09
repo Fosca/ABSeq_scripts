2bca1824b553003b7ffe8ca2a87bbd18e7c2dbb3
sam-planton <samuel.planton@gmail.com>
2020-04-09 17:42:42 +0200

    Adapté pour utiliser les fonctions de ABseq_func.stats_funcs

--- 99-linear_regressions_tests.py
+++ 99-linear_regressions_tests.py
@@ -11,10 +11,10 @@ from mpl_toolkits.axes_grid1 import make_axes_locatable
 from importlib import reload
 
 # ======================== REGRESSORS TO INCLUDE AND RESULTS FOLDER ========================= #
-Only_group_part = True
-analysis_name = 'test2'
+Only_group_part = True  # False means running the regressions at the individual level, True to just analyze and plot the group results
+analysis_name = 'MultipleChunkFactors'
 regress_path = op.join(config.result_path, 'linear_models', analysis_name)
-names = ["ChunkBeginning", "ChunkEnd", "ChunkDepth", "ChunkSize", "Identity"]
+names = ["Identity", "RepeatAlter", "ChunkNumber", "ChunkDepth", "OpenedChunks", "ChunkSize", "ChunkBeginning", "ChunkEnd"]
 
 if Only_group_part == False:
     # ========================= RUN LINEAR REGRESSION FOR EACH SUBJECT ========================== #
@@ -54,85 +54,82 @@ for name in names:
     exec(name + "_epo = mne.read_epochs(op.join(path, '" + name + "_epo.fif'))")
     betas[name] = globals()[name + '_epo']
 
-# ================= PLOT THE HEATMAPS OF THE GROUP-AVERAGED BETAS / CHANNEL ================ #
-fig_path = op.join(config.fig_path, 'Linear_regressions', analysis_name)
-utils.create_folder(fig_path)
-
-# Loop over the 3 ch_types
-plt.close('all')
-ch_types = ['eeg', 'grad', 'mag']
-for ch_type in ch_types:
-    fig, axes = plt.subplots(1, len(betas.keys()), figsize=(len(betas.keys())*4, 6), sharex=False, sharey=False, constrained_layout=True)
-    fig.suptitle(ch_type, fontsize=12, weight='bold')
-    ax = axes.ravel()[::1]
-    # Loop over the different betas
-    for x, regressor_name in enumerate(betas.keys()):
-        # ---- Data
-        evokeds = betas[regressor_name].average()
-        if ch_type == 'eeg':
-            betadata = evokeds.copy().pick_types(eeg=True, meg=False).data
-        elif ch_type == 'mag':
-            betadata = evokeds.copy().pick_types(eeg=False, meg='mag').data
-        elif ch_type == 'grad':
-            betadata = evokeds.copy().pick_types(eeg=False, meg='grad').data
-        minT = min(evokeds.times) * 1000
-        maxT = max(evokeds.times) * 1000
-        # ---- Plot
-        im = ax[x].imshow(betadata, origin='upper', extent=[minT, maxT, betadata.shape[0],0], aspect='auto', cmap='viridis') #cmap='RdBu_r'
-        ax[x].axvline(0, linestyle='-', color='black', linewidth=1)
-        for xx in range(3):
-            ax[x].axvline(250 * xx, linestyle='--', color='black', linewidth=0.5)
-        ax[x].set_xlabel('Time (ms)')
-        ax[x].set_ylabel('Channels')
-        ax[x].set_title(regressor_name, loc='center', weight='normal')
-        fig.colorbar(im, ax = ax[x], shrink=1, location='bottom')
-    fig_name = fig_path + op.sep + ('betas_' + ch_type + '.png')
-    print('Saving ' + fig_name)
-    plt.savefig(fig_name, dpi=300)
-    plt.close(fig)
-
-# =========================== PLOT THE BUTTERFLY OF THE REGRESSORS ========================== #
-ylim_eeg = 0.3
-ylim_mag = 20
-ylim_grad = 4
-# Butterfly plots for violations (one graph per sequence) - in EEG/MAG/GRAD
-ylim = dict(eeg=[-ylim_eeg, ylim_eeg], mag=[-ylim_mag, ylim_mag], grad=[-ylim_grad, ylim_grad])
-ts_args = dict(gfp=True, time_unit='s', ylim=ylim)
-topomap_args = dict(time_unit='s')
-times = 'peaks'
-for x, regressor_name in enumerate(betas.keys()):
-    evokeds = betas[regressor_name].average()
-    # EEG
-    fig = evokeds.plot_joint(ts_args=ts_args, title='EEG_' + regressor_name ,
-                                   topomap_args=topomap_args, picks='eeg', times=times, show=False)
-    fig_name = fig_path + op.sep + ('EEG_' + regressor_name + '.png')
-    print('Saving ' + fig_name)
-    plt.savefig(fig_name)
-    plt.close(fig)
-    # MAG
-    fig = evokeds.plot_joint(ts_args=ts_args, title='MAG_' + regressor_name,
-                                   topomap_args=topomap_args, picks='mag', times=times, show=False)
-    fig_name = fig_path + op.sep + ('MAG_' + regressor_name+'.png')
-    print('Saving ' + fig_name)
-    plt.savefig(fig_name)
-    plt.close(fig)
-    # #GRAD
-    fig = evokeds.plot_joint(ts_args=ts_args, title='GRAD_' + regressor_name,
-                                   topomap_args=topomap_args, picks='grad', times=times, show=False)
-    fig_name = fig_path + op.sep + ('GRAD_' + regressor_name + '.png')
-    print('Saving ' + fig_name)
-    plt.savefig(fig_name)
-    plt.close(fig)
+# # ================= PLOT THE HEATMAPS OF THE GROUP-AVERAGED BETAS / CHANNEL ================ #
+# fig_path = op.join(config.fig_path, 'Linear_regressions', analysis_name)
+# utils.create_folder(fig_path)
+#
+# # Loop over the 3 ch_types
+# plt.close('all')
+# ch_types = ['eeg', 'grad', 'mag']
+# for ch_type in ch_types:
+#     fig, axes = plt.subplots(1, len(betas.keys()), figsize=(len(betas.keys())*4, 6), sharex=False, sharey=False, constrained_layout=True)
+#     fig.suptitle(ch_type, fontsize=12, weight='bold')
+#     ax = axes.ravel()[::1]
+#     # Loop over the different betas
+#     for x, regressor_name in enumerate(betas.keys()):
+#         # ---- Data
+#         evokeds = betas[regressor_name].average()
+#         if ch_type == 'eeg':
+#             betadata = evokeds.copy().pick_types(eeg=True, meg=False).data
+#         elif ch_type == 'mag':
+#             betadata = evokeds.copy().pick_types(eeg=False, meg='mag').data
+#         elif ch_type == 'grad':
+#             betadata = evokeds.copy().pick_types(eeg=False, meg='grad').data
+#         minT = min(evokeds.times) * 1000
+#         maxT = max(evokeds.times) * 1000
+#         # ---- Plot
+#         im = ax[x].imshow(betadata, origin='upper', extent=[minT, maxT, betadata.shape[0],0], aspect='auto', cmap='viridis') #cmap='RdBu_r'
+#         ax[x].axvline(0, linestyle='-', color='black', linewidth=1)
+#         for xx in range(3):
+#             ax[x].axvline(250 * xx, linestyle='--', color='black', linewidth=0.5)
+#         ax[x].set_xlabel('Time (ms)')
+#         ax[x].set_ylabel('Channels')
+#         ax[x].set_title(regressor_name, loc='center', weight='normal')
+#         fig.colorbar(im, ax = ax[x], shrink=1, location='bottom')
+#     fig_name = fig_path + op.sep + ('betas_' + ch_type + '.png')
+#     print('Saving ' + fig_name)
+#     plt.savefig(fig_name, dpi=300)
+#     plt.close(fig)
+#
+# # =========================== PLOT THE BUTTERFLY OF THE REGRESSORS ========================== #
+# ylim_eeg = 0.3
+# ylim_mag = 20
+# ylim_grad = 4
+# # Butterfly plots for violations (one graph per sequence) - in EEG/MAG/GRAD
+# ylim = dict(eeg=[-ylim_eeg, ylim_eeg], mag=[-ylim_mag, ylim_mag], grad=[-ylim_grad, ylim_grad])
+# ts_args = dict(gfp=True, time_unit='s', ylim=ylim)
+# topomap_args = dict(time_unit='s')
+# times = 'peaks'
+# for x, regressor_name in enumerate(betas.keys()):
+#     evokeds = betas[regressor_name].average()
+#     # EEG
+#     fig = evokeds.plot_joint(ts_args=ts_args, title='EEG_' + regressor_name ,
+#                                    topomap_args=topomap_args, picks='eeg', times=times, show=False)
+#     fig_name = fig_path + op.sep + ('EEG_' + regressor_name + '.png')
+#     print('Saving ' + fig_name)
+#     plt.savefig(fig_name)
+#     plt.close(fig)
+#     # MAG
+#     fig = evokeds.plot_joint(ts_args=ts_args, title='MAG_' + regressor_name,
+#                                    topomap_args=topomap_args, picks='mag', times=times, show=False)
+#     fig_name = fig_path + op.sep + ('MAG_' + regressor_name+'.png')
+#     print('Saving ' + fig_name)
+#     plt.savefig(fig_name)
+#     plt.close(fig)
+#     # #GRAD
+#     fig = evokeds.plot_joint(ts_args=ts_args, title='GRAD_' + regressor_name,
+#                                    topomap_args=topomap_args, picks='grad', times=times, show=False)
+#     fig_name = fig_path + op.sep + ('GRAD_' + regressor_name + '.png')
+#     print('Saving ' + fig_name)
+#     plt.savefig(fig_name)
+#     plt.close(fig)
 
 # ======================= RUN CLUSTER STATISTICS ====================== #
 nperm = 5000  # number of permutations
-p1 = 0.05
-p2 = 0.01  # second p threshold, step-down-in-jumps test?
-# To perform a step-down-in-jumps test, pass a p-value for clusters to exclude from each successive iteration. Default is zero, perform no
-# step-down test (since no clusters will be smaller than this value). Setting this to a reasonable value, e.g. 0.05, can increase sensitivity
-# but costs computation time.
+threshold = None  # If threshold is None, t-threshold equivalent to p < 0.05 (if t-statistic)
+p_threshold = 0.05
 tmin = 0.0  # timewindow to test (crop data)
-tmax = 0.600 # timewindow to test (crop data)
+tmax = 0.600  # timewindow to test (crop data)
 fig_path = op.join(config.fig_path, 'Linear_regressions', analysis_name)
 utils.create_folder(fig_path)
 
@@ -142,90 +139,18 @@ for ch_type in ch_types:
     # Loop over the different regressors
     for x, regressor_name in enumerate(betas.keys()):
         data_condition = betas[regressor_name].copy()
-        fname = regressor_name
+        # fname = regressor_name
         # data_condition.apply_baseline(baseline=(-0.100, 0.0))  # baseline ?? (probably not at this step - betas)
         data_condition.crop(tmin=tmin, tmax=tmax)  # crop
-        connectivity = mne.channels.find_ch_connectivity(data_condition.info, ch_type=ch_type)[0]
-        if ch_type == 'eeg':
-            data = np.array([data_condition.pick_types(meg=False, eeg=True)[c].get_data() for c in range(len(data_condition))])
-        elif ch_type == 'mag':
-            data = np.array([data_condition.pick_types(meg=ch_type, eeg=False)[c].get_data() for c in range(len(data_condition))])
-        elif ch_type == 'grad':
-            data = np.array([data_condition.pick_types(meg=ch_type, eeg=False)[c].get_data() for c in range(len(data_condition))])
-        data = np.transpose(np.squeeze(data), (0, 2, 1))  # transpose for clustering
 
-
-        cluster_stats = permutation_cluster_1samp_test(data, threshold=None, n_jobs=6, verbose=True, tail=0, n_permutations=nperm,
-                                                   connectivity=connectivity, out_type='indices', check_disjoint=True, step_down_p=p1)
-
-
-        T_obs, clusters, p_values, _ = cluster_stats
-        good_cluster_inds = np.where(p_values < p2)[0]
-
-
-
-        print("Good clusters: %s" % good_cluster_inds)
+        # RUN THE STATS
+        cluster_stats, data_array_chtype, _ = stats_funcs.run_cluster_permutation_test_1samp(data_condition, ch_type=ch_type, nperm=nperm, threshold=threshold, n_jobs=6, tail=0)
+        cluster_info = stats_funcs.extract_info_cluster(cluster_stats, p_threshold, data_condition, data_array_chtype, ch_type)
 
         # PLOT CLUSTERS
-        plt.close('all')
-        # set_matplotlib_defaults()
-        times = data_condition.times * 1e3
-        colors = 'r', 'steelblue'
-        linestyles = '-', '--'
-        pos = mne.find_layout(data_condition.info).pos
-        T_obs_max = 5.
-        T_obs_min = -T_obs_max
-        # loop over significant clusters
-        for i_clu, clu_idx in enumerate(good_cluster_inds):
-            # unpack cluster information, get unique indices
-            time_inds, space_inds = np.squeeze(clusters[clu_idx])
-            ch_inds = np.unique(space_inds)
-            time_inds = np.unique(time_inds)
-            # get topography for T0 stat
-            T_obs_map = T_obs[time_inds, ...].mean(axis=0)
-            # get signals at significant sensors
-            signals = data[..., ch_inds].mean(axis=-1)
-            sig_times = times[time_inds]
-            # create spatial mask
-            mask = np.zeros((T_obs_map.shape[0], 1), dtype=bool)
-            mask[ch_inds, :] = True
-            # initialize figure
-            fig, ax_topo = plt.subplots(1, 1, figsize=(7, 2.))
-            # plot average test statistic and mark significant sensors
-            image, _ = plot_topomap(T_obs_map, pos, mask=mask, axes=ax_topo,
-                                    vmin=T_obs_min, vmax=T_obs_max,
-                                    show=False)
-            # advanced matplotlib for showing image with figure and colorbar
-            # in one plot
-            divider = make_axes_locatable(ax_topo)
-            # add axes for colorbar
-            ax_colorbar = divider.append_axes('right', size='5%', pad=0.05)
-            plt.colorbar(image, cax=ax_colorbar, format='%0.1f')
-            ax_topo.set_xlabel('Averaged t-map\n({:0.1f} - {:0.1f} ms)'.format(
-                *sig_times[[0, -1]] ))
-            # ax_topo.annotate(chr(65 + 2 * i_clu), (0.1, 1.1), **annot_kwargs)
-            # add new axis for time courses and plot time courses
-            ax_signals = divider.append_axes('right', size='300%', pad=1.2)
-            for signal, name, col, ls in zip(signals, [fname], colors, linestyles):
-                ax_signals.plot(times, signal * 1e6, color=col, linestyle=ls, label=name)
-            # add information
-            ax_signals.axvline(0, color='k', linestyle=':', label='stimulus onset')
-            ax_signals.set_xlim([times[0], times[-1]])
-            ax_signals.set_xlabel('Time [ms]')
-            ax_signals.set_ylabel('Amplitude [uV]')
-            # plot significant time range
-            ymin, ymax = ax_signals.get_ylim()
-            ax_signals.fill_betweenx((ymin, ymax), sig_times[0], sig_times[-1], color='orange', alpha=0.3)
-            ax_signals.legend(loc='lower right')
-            title = 'Cluster #{0} (p < {1:0.3f})'.format(i_clu + 1, p_values[clu_idx])
-            ax_signals.set(ylim=[ymin, ymax], title=title)
-            # ax_signals.annotate(chr(65 + 2 * i_clu + 1), (-0.125, 1.1), **annot_kwargs)
-            # clean up viz
-            fig.tight_layout(pad=0.5, w_pad=0)
-            fig.subplots_adjust(bottom=.05)
-            fig_name = fig_path + op.sep + 'stats_' + ch_type + '_' + regressor_name + '_clust_' + str(i_clu+1) + '.png'
-            print('Saving ' + fig_name)
-            plt.savefig(fig_name, dpi=300)
+        figname_initial = fig_path + op.sep + 'stats_' + ch_type + '_' + regressor_name
+        stats_funcs.plot_clusters(cluster_stats, p_threshold, data_condition, data_array_chtype, ch_type, T_obs_max=5.,
+                                  fname=analysis_name, figname_initial=figname_initial)
 
         # =========================================================== #
         # ==========  cluster evoked data plot
@@ -234,11 +159,15 @@ for ch_type in ch_types:
         # ------------------ LOAD THE EVOKED FOR THE CURRENT REGRESSOR ------------ #
         evoked_reg = evoked_funcs.load_evoked(subject='all', filter_name=regressor_name, filter_not=None, cleaned=True)
 
+        T_obs, clusters, p_values, _ = cluster_stats
+        good_cluster_inds = np.where(p_values < p_threshold)[0]
+        print("Good clusters: %s" % good_cluster_inds)
+
         for i_clu, clu_idx in enumerate(good_cluster_inds):
 
             # ----------------- SELECT CHANNELS OF INTEREST ----------------- #
             time_inds, space_inds = np.squeeze(clusters[clu_idx])
-            ch_inds = np.unique(space_inds)  # list of channels we want to average and plot (!? after pick_types or from all ?!)
+            ch_inds = np.unique(space_inds)  # list of channels we want to average and plot (!should be from one ch_type!)
 
             # ----------------- PLOT ----------------- #
             fig, ax = plt.subplots(1, 1, figsize=(10, 5))
@@ -248,9 +177,15 @@ for ch_type in ch_types:
                 plt.axvline(250 * xx, linestyle='--', color='black', linewidth=0.5)
             ax.set_xlabel('Time (ms)')
             condnames = list(evoked_reg.keys())
-            for condname in condnames:
+            if len(condnames) == 2:
+                colorslist = ['r', 'b']
+            else:
+                NUM_COLORS = len(condnames)
+                cm = plt.get_cmap('jet')
+                colorslist = ([cm(1. * i / (NUM_COLORS - 1)) for i in range(NUM_COLORS)])
+            for ncond, condname in enumerate(condnames):
                 data = evoked_reg[condname].copy()
-                evoked_funcs.plot_evoked_with_sem_1cond(data, condname, ch_inds, color=None, filter=True, axis=None)
+                evoked_funcs.plot_evoked_with_sem_1cond(data, condname, ch_type, ch_inds, color=colorslist[ncond], filter=True, axis=None)
             plt.legend(loc='upper right', fontsize=9)
             ax.set_xlim([-100, 750])
             plt.title(ch_type + '_' + regressor_name + '_clust_' + str(i_clu+1))
