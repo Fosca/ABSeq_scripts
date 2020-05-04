94021a7b8c077e388b82277a38ec2bf0274e00e7
sam-planton <samuel.planton@gmail.com>
2020-05-04 20:49:31 +0200

    Functions for source estimation (alpha version)

---
+++ 12-source_estimation.py
@@ -0,0 +1,78 @@
+import mne
+import os
+import config
+import matplotlib.pyplot as plt
+import os.path as op
+from ABseq_func import *
+
+# =========== FREESURFER MRI RECONSTRUCTION MUST BE DONE BEFORE =========== #
+# see freesurfer_recon_all.sh
+
+# =========== DO THIS BEFORE LAUNCHING PYTHON =========== #
+# source $FREESURFER_HOME/SetUpFreeSurfer.sh
+# required for mne.bem.make_watershed_bem
+
+# TEST FREESURFER RECON-ALL IN PYTHON SCRIPT?? // no luck..
+# ( $FREESURFER_HOME/SetUpFreeSurfer.sh)
+# source $FREESURFER_HOME/SetUpFreeSurfer.sh
+# OR TEST SOMETHIN LIKE:
+# os.environ["$FREESURFER_HOME"] = '/i2bm/local/freesurfer'
+# os.system('export SUBJECTS_DIR=/neurospin/meg/meg_tmp/ABSeq_Samuel_Fosca2019/data/MRI')
+# os.system('export SUBJECT=sub01-pa_190002')
+# os.system('$FREESURFER_HOME/SetUpFreeSurfer.sh')
+# os.system('source $FREESURFER_HOME/SetUpFreeSurfer.sh')
+# os.system('export \PTK=/neurospin/meg/meg_tmp/ABSeq_Samuel_Fosca2019/data/MRI/sub01-pa_190002/*.nii')
+# os.system('recon-all -s $SUBJECT -i $PTK -all')
+# os.environ["$FREESURFER_HOME"] = '/i2bm/local/freesurfer'
+# os.environ["SUBJECTS_DIR"] = fsMRI_dir
+# mne.set_config('$FREESURFER_HOME', '/i2bm/local/freesurfer', set_env=True)
+
+fsMRI_dir = op.join(config.root_path, 'data', 'MRI', 'fs_converted')
+subject = config.subjects_list[1]
+
+# source_estimation_funcs.prepare_bem(subject, fsMRI_dir)
+source_estimation_funcs.create_source_space(subject, fsMRI_dir)
+source_estimation_funcs.forward_solution(subject, fsMRI_dir)
+source_estimation_funcs.inverse_operator(subject)
+source_estimation_funcs.source_estimates(subject, 'items_standard_all', evoked_filter_not=None)
+
+
+def old_script_stuff():
+    # TEST PLOT SOURCES
+    fsMRI_dir = op.join(config.root_path, 'data', 'MRI', 'fs_converted')
+    subject = config.subjects_list[0]
+    meg_subject_dir = op.join(config.meg_dir, subject)
+    stc = mne.read_source_estimate(op.join(meg_subject_dir, '%s_mne_dSPM_inverse-%s' % (subject, 'test')))
+
+    stc.plot(subject=subject, subjects_dir=fsMRI_dir, time_viewer=True)
+
+    evoked[0].plot()
+
+    stc.plot(views='lat', hemi='split', size=(800, 400), subject=subject,
+             subjects_dir=fsMRI_dir, initial_time=0.0,
+             time_viewer=True)
+
+    morph = mne.compute_source_morph(stc, subject_from=subject, subjects_dir=fsMRI_dir, subject_to='fsaverage')
+    stc_fsaverage = morph.apply(stc)
+    stc_fsaverage.plot()
+
+    stc.plot(subjects_dir=fsMRI_dir)
+
+    # ALTERNATIVE
+    # Create BEM surfaces
+    # see https://mne.tools/stable/auto_tutorials/source-modeling/plot_forward.html#sphx-glr-auto-tutorials-source-modeling-plot-forward-py
+    mne.bem.make_watershed_bem(subject, overwrite=True)
+
+    # Plot BEM surfaces
+    tmp = mne.viz.plot_bem(subject=subject, subjects_dir=fsMRI_dir, brain_surfaces='white', orientation='coronal')
+    plt.savefig(op.join(fsMRI_dir, subject, 'bem_fig.png'), dpi=300)
+
+    # Coregistration? (manually)
+    # see https://www.slideshare.net/mne-python/mnepython-coregistration
+    mne.gui.coregistration(subject=subject, subjects_dir=fsMRI_dir)
+
+    #
+    fname_trans = op.join(config.meg_dir, subject, subject + '-trans.fif')
+    datafile = op.join(config.meg_dir, subject, 'run01_raw.fif')
+    info = mne.io.read_info(datafile)
+    mne.viz.plot_alignment(info, fname_trans, subject=subject, dig=True, meg=['helmet', 'sensors'], subjects_dir=fsMRI_dir, surfaces='head')
---
+++ ABseq_func/source_estimation_funcs.py
@@ -0,0 +1,92 @@
+import mne
+import config
+import matplotlib.pyplot as plt
+import os.path as op
+from ABseq_func import *
+
+# =========== DO THIS BEFORE LAUNCHING PYTHON =========== #
+# required for mne.bem.make_watershed_bem
+# source $FREESURFER_HOME/SetUpFreeSurfer.sh
+
+# see https://mne.tools/stable/overview/cookbook.html#setting-up-source-space
+# and https://github.com/brainthemind/CogBrainDyn_MEG_Pipeline
+
+def prepare_bem(subject, fsMRI_dir):
+    meg_subject_dir = op.join(config.meg_dir, subject)
+
+    # Create BEM surfaces from T1 MRI using freesurfer watershed
+    print('Subject ' + subject + ': make_watershed_bem ======================')
+    mne.bem.make_watershed_bem(subject=subject, subjects_dir=fsMRI_dir, overwrite=True)
+
+    # BEM model meshes (alternative to freesurfer watershed?)
+    model = mne.make_bem_model(subject=subject, subjects_dir=fsMRI_dir)
+    mne.write_bem_surfaces(op.join(meg_subject_dir, subject + '-5120-5120-5120-bem.fif'), model)
+
+    # BEM solution
+    bem_sol = mne.make_bem_solution(model)
+    mne.write_bem_solution(op.join(meg_subject_dir, subject + '-5120-5120-5120-bem-sol.fif'), bem_sol)
+
+
+def create_source_space(subject, fsMRI_dir):
+    meg_subject_dir = op.join(config.meg_dir, subject)
+    # Create source space
+    src = mne.setup_source_space(subject, spacing=config.spacing, subjects_dir=fsMRI_dir)
+    mne.write_source_spaces(op.join(meg_subject_dir, subject + '-oct6-src.fif'), src)
+
+
+def forward_solution(subject, fsMRI_dir):
+    meg_subject_dir = op.join(config.meg_dir, subject)
+    # Load some evoked data (just for info?)
+    fname_evoked = op.join(meg_subject_dir, 'evoked_cleaned', 'items_standard_all-ave.fif')
+    evoked = mne.read_evokeds(fname_evoked)
+    # BEM solution
+    fname_bem = op.join(meg_subject_dir, '%s-5120-5120-5120-bem-sol.fif' % subject)
+    # Forward solution
+    fwd = mne.make_forward_solution(evoked[0].info, fname_trans, src, fname_bem, mindist=config.mindist)
+    extension = '_%s-fwd' % (config.spacing)
+    fname_fwd = op.join(meg_subject_dir, subject + config.base_fname.format(**locals()))
+    mne.write_forward_solution(fname_fwd, fwd, overwrite=True)
+
+
+def compute_noise_cov(subject):
+    meg_subject_dir = op.join(config.meg_dir, subject)
+    # Noise covariance
+    emptyroom = mne.io.read_raw_fif(op.join(config.meg_dir, subject, 'empty_room_raw.fif'), allow_maxshield=True)  # is it correct to do this??
+    cov = mne.compute_raw_covariance(emptyroom)
+    return cov
+
+
+def inverse_operator(subject):
+    meg_subject_dir = op.join(config.meg_dir, subject)
+
+    # Noise covariance
+    cov = compute_noise_cov(subject)
+    # Load some evoked data (just for info?)
+    fname_evoked = op.join(meg_subject_dir, 'evoked_cleaned', 'items_standard_all-ave.fif')
+    evoked = mne.read_evokeds(fname_evoked)
+    # Load forward solution
+    extension = '_%s-fwd' % (config.spacing)
+    fname_fwd = op.join(meg_subject_dir, subject + config.base_fname.format(**locals()))
+    forward = mne.read_forward_solution(fname_fwd)
+    # Inverse operator
+    inverse_operator = mne.minimum_norm.make_inverse_operator(evoked[0].info, forward, cov, loose=0.2, depth=0.8)
+    extension = '_%s-inv' % (config.spacing)
+    fname_inv = op.join(meg_subject_dir, subject + config.base_fname.format(**locals()))
+    mne.minimum_norm.write_inverse_operator(fname_inv, inverse_operator)
+
+
+def source_estimates(subject, evoked_filter_name, evoked_filter_not=None):
+    meg_subject_dir = op.join(config.meg_dir, subject)
+
+    # Load evoked
+    evoked = evoked_funcs.load_evoked(subject='all', filter_name=evoked_filter_name + '_allseq', evoked_filter_not=None, cleaned=True)
+    # Load inverse operator
+    extension = '_%s-inv' % (config.spacing)
+    fname_inv = op.join(meg_subject_dir, subject + config.base_fname.format(**locals()))
+    inverse_operator = mne.minimum_norm.read_inverse_operator(fname_inv)
+
+    # Source estimates: apply inverse
+    snr = 3.0
+    lambda2 = 1.0 / snr ** 2
+    stc = mne.minimum_norm.apply_inverse(evoked[0], inverse_operator, lambda2, "dSPM", pick_ori=None)
+    stc.save(op.join(meg_subject_dir, subject + '_dSPM_inverse_' + evoked_filter_name))
