b0d8de6cf3598c975afc196ad13bd3011614e12a
Fosca <fosca.alroumi@gmail.com>
2020-09-22 12:13:56 +0200

    trying to fix apply_SVM_filter_16_items_epochs

--- ABseq_func/SVM_funcs.py
+++ ABseq_func/SVM_funcs.py
@@ -426,7 +426,7 @@ def apply_SVM_filter_16_items_epochs(subject, times=[x / 1000 for x in range(0, 
                         for mm, point_of_interest in enumerate(points):
                             print(" The point of interest has index %i"%point_of_interest)
                             print(" === MAKE SURE THAT WHEN SELECTING SVM_to_data[point_of_interest,:] WE ARE INDEED CHOOSING THE TRAINING TIMES ===" )
-                            epochs_1st_sens_m_filtered_data = SVM_to_data[:,point_of_interest]
+                            epochs_1st_sens_m_filtered_data = SVM_to_data[point_of_interest,:]
                             print('epochs_1st_sens_m_filtered_data has shape')
                             print(epochs_1st_sens_m_filtered_data.shape)
                             print("data_for_epoch_object has shape")
@@ -441,7 +441,7 @@ def apply_SVM_filter_16_items_epochs(subject, times=[x / 1000 for x in range(0, 
                     else:
                         print(
                             " === MAKE SURE THAT WHEN SELECTING SVM_to_data[np.min(points):np.max(points),:] WE ARE INDEED CHOOSING THE TRAINING TIMES ===")
-                        epochs_1st_sens_m_filtered_data = np.mean(SVM_to_data[0,:,np.min(points):np.max(points)],axis=-1)
+                        epochs_1st_sens_m_filtered_data = np.mean(SVM_to_data[np.min(points):np.max(points),:],axis=-1)
                         data_for_epoch_object[counter, :] = np.squeeze(epochs_1st_sens_m_filtered_data)
                         metadata_m = epochs_1st_sens_m.metadata
                         metadata_m['SVM_filter_min_datapoint'] = np.min(points)
