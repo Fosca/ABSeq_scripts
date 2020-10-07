a97f584c84876742d805597983d6ab03f708089e
Samuel <samuel.planton@gmail.com>
2020-10-07 17:14:15 +0200

    Corrections, cleanup

--- 12-source_estimation.py
+++ 12-source_estimation.py
@@ -15,37 +15,97 @@ from mpl_toolkits.axes_grid1 import make_axes_locatable, ImageGrid
 # source $FREESURFER_HOME/SetUpFreeSurfer.sh
 # required for mne.bem.make_watershed_bem
 
-# TEST FREESURFER RECON-ALL IN PYTHON SCRIPT?? // no luck..
-# source $FREESURFER_HOME/SetUpFreeSurfer.sh
-# os.environ["$FREESURFER_HOME"] = '/i2bm/local/freesurfer'
-# os.system('export SUBJECTS_DIR=/neurospin/meg/meg_tmp/ABSeq_Samuel_Fosca2019/data/MRI')
-# os.system('export SUBJECT=sub01-pa_190002')
-# os.system('$FREESURFER_HOME/SetUpFreeSurfer.sh')
-# os.system('source $FREESURFER_HOME/SetUpFreeSurfer.sh')
-# os.system('export \PTK=/neurospin/meg/meg_tmp/ABSeq_Samuel_Fosca2019/data/MRI/sub01-pa_190002/*.nii')
-# os.system('recon-all -s $SUBJECT -i $PTK -all')
-# os.environ["SUBJECTS_DIR"] = fsMRI_dir
-# mne.set_config('$FREESURFER_HOME', '/i2bm/local/freesurfer', set_env=True)
-
 fsMRI_dir = op.join(config.root_path, 'data', 'MRI', 'fs_converted')
-subject = config.subjects_list[1]
+os.environ["SUBJECTS_DIR"] = fsMRI_dir
 
-# Create BEM surfaces & solution
-source_estimation_funcs.prepare_bem(subject, fsMRI_dir)
+subjects_list = config.subjects_list
+subjects_list = [config.subjects_list[i] for i in [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 13, 14, 15, 16]]
 
+# ========================================================================================================================== #
+# Prepare surfaces and coregistration
+# ========================================================================================================================== #
+for subject in subjects_list:
+
+    # Remove temp files that may block the make_bem_model function ??
+    fname = op.join(config.scripts_path, 'brain')
+    if os.path.exists(fname):
+        os.remove(fname)
+    fname = op.join(config.scripts_path, 'inner_skull')
+    if os.path.exists(fname):
+        os.remove(fname)
+    fname = op.join(config.scripts_path, 'outer_skin')
+    if os.path.exists(fname):
+        os.remove(fname)
+    fname = op.join(config.scripts_path, 'outer_skull')
+    if os.path.exists(fname):
+        os.remove(fname)
+
+    print(subject)
+    # Create BEM surfaces & solution
+    source_estimation_funcs.prepare_bem(subject, fsMRI_dir)
+
+# /!\ GUI for the coregistration /!\
 # Create coregistration "-trans.fif" file
+subject = config.subjects_list[11]
 mne.gui.coregistration(subject=subject, subjects_dir=fsMRI_dir)
 
-# Source space, forward & inverse
-source_estimation_funcs.create_source_space(subject, fsMRI_dir)
-source_estimation_funcs.forward_solution(subject, fsMRI_dir)
-source_estimation_funcs.inverse_operator(subject)
+# ========================================================================================================================== #
+# Compute source space & inverse solution
+# ========================================================================================================================== #
+for subject in subjects_list:
+    # Save bem fig
+    fig = mne.viz.plot_bem(subject=subject, subjects_dir=fsMRI_dir, brain_surfaces='white', orientation='coronal', show=False)
+    fig.savefig(op.join(fsMRI_dir, subject, 'bem.jpg'), dpi=300)
+    plt.close(fig)
 
-# Estimate sources from evoked
-source_estimation_funcs.source_estimates(subject,  evoked_filter_name='items_standard_all', evoked_filter_not=None)
-source_estimation_funcs.source_estimates(subject,  evoked_filter_name='items_viol_all', evoked_filter_not=None)
+    # Source space, forward & inverse
+    source_estimation_funcs.create_source_space(subject, fsMRI_dir)
+    source_estimation_funcs.forward_solution(subject, fsMRI_dir)
+    source_estimation_funcs.inverse_operator(subject)
+
+
+def script_sanitycheck_plotactivation():
+    outfold = op.join(config.fig_path, 'sources_activation')
+    utils.create_folder(outfold)
+    subjects_list = [config.subjects_list[i] for i in [0, 1, 4, 6, 7, 8, 9, 10, 13, 14, 15, 16]]
+    time_init = 0.170
+    for subject in subjects_list:
+        source_estimation_funcs.source_estimates(subject, evoked_filter_name='items_standard_all', evoked_filter_not=None)
+        stc = mne.read_source_estimate(op.join(config.meg_dir, subject, 'evoked_cleaned', 'items_standard_all_dSPM_inverse'))
+        morph = mne.compute_source_morph(stc, subject_from=subject, subjects_dir=fsMRI_dir, subject_to='fsaverage')
+        stc_fsaverage = morph.apply(stc)
+        brain = stc_fsaverage.plot(views=['lat'], surface='inflated', hemi='split', size=(1200, 600), subject='fsaverage', clim=dict(kind='value', lims=[3, 9, 15]),
+                           subjects_dir=fsMRI_dir, initial_time=time_init, smoothing_steps=5, show_traces=False, time_viewer=False)
+        screenshot = brain.screenshot()
+        brain.close()
+        nonwhite_pix = (screenshot != 255).any(-1)
+        nonwhite_row = nonwhite_pix.any(1)
+        nonwhite_col = nonwhite_pix.any(0)
+        cropped_screenshot = screenshot[nonwhite_row][:, nonwhite_col]
+        fig = plt.imshow(cropped_screenshot)
+        plt.axis('off')
+        plt.title(subject + ': Evoked items_standard_all, at ' + str('%d' % (time_init*1000)) + ' ms')
+        plt.savefig(op.join(outfold, subject + '_items_standard_all_' + str('%d' % (time_init*1000)) + 'ms.png'), dpi=600)
+        plt.close('all')
+
 
 def script_plot_sources():
+    method = "dSPM"
+    stc = mne.read_source_estimate(op.join(config.meg_dir, subject, 'evoked_cleaned', 'items_standard_all_dSPM_inverse'))
+    fig, ax = plt.subplots()
+    ax.plot(1e3 * stc.times, stc.data[::100, :].T)
+    ax.set(xlabel='time (ms)', ylabel='%s value' % method)
+    vertno_max, time_max = stc.get_peak(hemi='lh')
+    surfer_kwargs = dict(
+        surface='pial', hemi='split', subjects_dir=fsMRI_dir, subject=subject,
+        clim=dict(kind='value', lims=[8, 12, 15]), views='lateral',
+        initial_time=time_max, time_unit='s', size=(800, 800), smoothing_steps=10)
+    brain = stc.plot(**surfer_kwargs)
+    brain.add_foci(vertno_max, coords_as_verts=True, hemi='lh', color='blue',
+                   scale_factor=0.6, alpha=0.5)
+    brain.add_text(0.1, 0.9, 'dSPM (plus location of maximal activation)', 'title',
+                   font_size=14)
+
     meg_subject_dir = op.join(config.meg_dir, subject)
     # Plot evoked & sources
     plt.close('all')
@@ -71,8 +131,7 @@ def script_plot_sources():
     plt.close('all')
     evoked = evoked2
     stc = stc2
-    max_t = max_t2 #0.070  # max_t2
-
+    max_t = max_t2  # 0.070  # max_t2
 
     colormap = 'hot'
     clim = dict(kind='percent', lims=[0.3, 0.65, 1.0])
