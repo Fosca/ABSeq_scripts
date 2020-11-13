de794fb4b400c1e4c2bdbdab8a4848bf1831fc41
Fosca <fosca.alroumi@gmail.com>
2020-11-13 14:53:50 +0100

    changement dans load_resid_epochs_items qui charge maintenant au bon endroit

--- run_cluster.py
+++ run_cluster.py
@@ -13,10 +13,10 @@ print('jaime les tomates')
 # rerun this once autoreject has cleaned the epochs
 # cluster_funcs.create_qsub('compute_evoked', 'evo', 'evo', queue='Nspin_bigM')
 # cluster_funcs.create_qsub('SVM_analysis', 'svm', 'svm', queue='Nspin_bigM')
-cluster_funcs.create_qsub('SVM_features', 'svm_f', 'svm_f', queue='Nspin_long')
+cluster_funcs.create_qsub('SVM_features', 'svm_f', 'svm_coucou', queue='Nspin_long')
 cluster_funcs.create_qsub('SVM_1', 'svm1', 'svm1', queue='Nspin_long')
-cluster_funcs.create_qsub('SVM_2', 'svm2', 'svm2', queue='Nspin_long')
-cluster_funcs.create_qsub('SVM_3', 'svm3', 'svm3', queue='Nspin_long')
+# cluster_funcs.create_qsub('SVM_2', 'svm2', 'svm2', queue='Nspin_long')
+# cluster_funcs.create_qsub('SVM_3', 'svm3', 'svm3', queue='Nspin_long')
 
 
 # cluster_funcs.create_qsub('EMS', 'ems', 'ems', queue='Nspin_bigM', sublist_subjects=['sub16-ma_190185'])
