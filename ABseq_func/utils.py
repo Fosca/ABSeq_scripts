04775ceb54036109ac2e1f5474c6b826d2df2538
Fosca <fosca.alroumi@gmail.com>
2021-02-08 17:03:28 +0100

    added read info in utils

--- ABseq_func/utils.py
+++ ABseq_func/utils.py
@@ -1,5 +1,18 @@
 import os
+import config
+import mne
 
 def create_folder(folder_name):
     if not os.path.exists(folder_name):
         os.makedirs(folder_name)
+
+def load_info_subjects(subjects_list):
+
+    infos = []
+    for subject in subjects_list:
+        evoked_path = config.meg_dir+'/'+subject+'/evoked_cleaned/items_standard_all-ave.fif'
+        ev = mne.read_evokeds(evoked_path)
+        infos.append(ev.info)
+
+    return infos
+
