73c4283c00c7cdd6e8841f34ba389bee199c5797
sam-planton <samuel.planton@gmail.com>
2021-02-12 12:09:37 +0100

    Updated initialization_paths

--- initialization_paths/initialization_paths.py
+++ initialization_paths/initialization_paths.py
@@ -5,5 +5,13 @@ sys.path.append("/neurospin/meg/meg_tmp/ABSeq_Samuel_Fosca2019/scripts/Transitio
 sys.path.append("/neurospin/meg/meg_tmp/ABSeq_Samuel_Fosca2019/scripts/groupmne/")
 sys.path.append("Z:/scripts/TransitionProbModel/")
 sys.path.append("Z:/scripts/groupmne/")
+sys.path.append("W:/LocalABseq_scripts/TransitionProbModel/")
+sys.path.append("W:/LocalABseq_scripts/groupmne/")
 sys.path.append("//Users/fosca/Documents/Fosca/Post_doc/Projects/ABSeq/scripts/TransitionProbModel/")
 import MarkovModel_Python
+
+path = "/neurospin/meg/meg_tmp/Geom_Seq_Fosca_2017/Geom_Seq_scripts/Analysis_scripts/packages/"
+list_packages = ['jr-tools']
+
+for package in list_packages:
+    sys.path.append(path+package)
