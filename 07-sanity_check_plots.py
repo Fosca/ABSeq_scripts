607f233a844aace5de1432b4b206e86c425593b6
sam-planton <samuel.planton@gmail.com>
2021-05-27 13:30:07 +0200

    Minor changes

--- 07-sanity_check_plots.py
+++ 07-sanity_check_plots.py
@@ -10,26 +10,26 @@ def make_figures(subject):
     # PLOTS - Items epochs
     # ----------------------------------------------------------------------------------------------------------- #
 
-    # -- LOAD THE EPOCHS -- #
-    epochs = epoching_funcs.load_epochs_items(subject, cleaned=True, AR_type='local')
-
-    # -- PLOTS -- #
-    evoked_funcs.plot_butterfly_items(epochs, subject, violation_or_not=1, apply_baseline=True)  # items epoch are not already baseline corrected
-    evoked_funcs.plot_butterfly_items(epochs, subject, violation_or_not=0, apply_baseline=True)
-    GFP_funcs.plot_gfp_items_standard_or_deviants(epochs, subject, h_freq=None, standard_or_deviant='standard')
-    GFP_funcs.plot_gfp_items_standard_or_deviants(epochs, subject, h_freq=None, standard_or_deviant='deviant')
+    # # -- LOAD THE EPOCHS -- #
+    # epochs = epoching_funcs.load_epochs_items(subject, cleaned=True, AR_type='local')
+    #
+    # # -- PLOTS -- #
+    # evoked_funcs.plot_butterfly_items(epochs, subject, violation_or_not=1, apply_baseline=True)  # items epoch are not already baseline corrected
+    # evoked_funcs.plot_butterfly_items(epochs, subject, violation_or_not=0, apply_baseline=True)
+    # GFP_funcs.plot_gfp_items_standard_or_deviants(epochs, subject, h_freq=None, standard_or_deviant='standard')
+    # GFP_funcs.plot_gfp_items_standard_or_deviants(epochs, subject, h_freq=None, standard_or_deviant='deviant')
 
     # ----------------------------------------------------------------------------------------------------------- #
     # PLOTS - Full-sequence epochs
     # ----------------------------------------------------------------------------------------------------------- #
 
     # -- LOAD THE EPOCHS -- #
-    epochs = epoching_funcs.load_epochs_full_sequence(subject, cleaned=True, AR_type='local')
+    epochs = epoching_funcs.load_epochs_full_sequence(subject, cleaned=True, AR_type='global')
 
     # -- PLOTS -- #
-    evoked_funcs.plot_butterfly_first_item(epochs, subject, apply_baseline=False)  # fullseq epoch are already baseline corrected
-    GFP_funcs.plot_gfp_full_sequence_standard(epochs, subject, h_freq=None)
-    GFP_funcs.plot_gfp_full_sequence_deviants_4pos(epochs, subject, h_freq=None)
+    evoked_funcs.plot_butterfly_first_item(epochs, subject, apply_baseline=False, ch_types = ['grad', 'mag'])  # fullseq epoch are already baseline corrected
+    GFP_funcs.plot_gfp_full_sequence_standard(epochs, subject, h_freq=None, ch_types = ['grad', 'mag'])
+    GFP_funcs.plot_gfp_full_sequence_deviants_4pos(epochs, subject, h_freq=None, ch_types = ['grad', 'mag'])
 
 parallel, run_func, _ = parallel_func(make_figures, n_jobs=config.N_JOBS)
 parallel(run_func(subject) for subject in config.subjects_list)
