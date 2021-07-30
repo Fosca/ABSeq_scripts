70cc6d5f15d9c0824a5c82655cdb98ebd75d74be
sam-planton <samuel.planton@gmail.com>
2021-07-30 18:13:47 +0200

    Minor corrections

--- 10-linear_regressions_fosca.py
+++ 10-linear_regressions_fosca.py
@@ -3,16 +3,17 @@ sys.path.append("/neurospin/meg/meg_tmp/ABSeq_Samuel_Fosca2019/scripts/ABSeq_scr
 import initialization_paths
 from ABseq_func import regression_funcs
 import config
-subject = config.subjects_list[0]
+subject = config.subjects_list[10]
 
 filter_names = ['Hab','Stand','Viol']
 
 # --- update the metadata fields for the epochs (clean and dirty) and save them again ----
-# regression_funcs.update_metadata_epochs_and_save_epochs(subject)
+regression_funcs.update_metadata_epochs_and_save_epochs(subject)
 
 for filter_name in filter_names:
+    regression_funcs.compute_regression(subject,['Complexity'],"",filter_name)
     regression_funcs.compute_regression(subject,['Intercept','surprise_100','Surprisenp1','RepeatAlter','RepeatAlternp1'],"",filter_name)
-    regression_funcs.compute_regression(subject,['Complexity'],"/Hab/Intercept_surprise_100_Surprisenp1_RepeatAlter_RepeatAlternp1/"+subject+"-residuals-baselined_clean-epo.fif",filter_name)
+    regression_funcs.compute_regression(subject,['Complexity'],"/Intercept_surprise_100_Surprisenp1_RepeatAlter_RepeatAlternp1/"+subject+"/residuals--remapped_clean-epo.fif",filter_name)
 
 
 
