d5b957e4c7af60552f337ad3761328f2b6b5419f
Fosca <fosca.alroumi@gmail.com>
2021-08-11 14:36:23 +0200

    linear reg of structure on residuals

--- ABseq_func/cluster_funcs.py
+++ ABseq_func/cluster_funcs.py
@@ -275,16 +275,13 @@ def ord_code_16items(subject,load_residuals_regression=False):
 #                            LINEAR REGRESSIONS
 # ----------------------------------------------------------------------------------------------------------------------
 
-
-
-
 def linear_reg(subject):
     from ABseq_func import regression_funcs
     filter_names = ['Hab', 'Stand', 'Viol']
     # regression_funcs.update_metadata_epochs_and_save_epochs(subject)
     for filter_name in filter_names:
         # regressors_names = ['Complexity','SequenceID','WithinChunkPosition','RepeatAlter','ChunkBeginning', 'ChunkEnd', 'ChunkNumber', 'ChunkDepth']
-        # epochs_fname = "/Intercept_surprise_100_Surprisenp1_RepeatAlter_RepeatAlternp1/"+subject+"/residuals--clean-epo.fif"
+        # epochs_fname = "/Intercept_surprise_100_Surprisenp1_RepeatAlter_RepeatAlternp1/"+subject+"/residuals--remapped_clean-epo.fif"
         # remap_grads=True
         # filter = ''
         # subject = config.subjects_list[2]
@@ -293,8 +290,8 @@ def linear_reg(subject):
         # apply_baseline=False
         # suffix = ''
         # filter_name = 'Hab'
-        regression_funcs.compute_regression(subject,['Intercept','surprise_100','Surprisenp1','RepeatAlter','RepeatAlternp1'],"",filter_name,remap_grads=True)
-        regression_funcs.compute_regression(subject,['Complexity','SequenceID','WithinChunkPosition','RepeatAlter','ChunkBeginning', 'ChunkEnd', 'ChunkNumber', 'ChunkDepth'],"/Intercept_surprise_100_Surprisenp1_RepeatAlter_RepeatAlternp1/"+subject+"/residuals--remapped_clean-epo.fif",filter_name,remap_grads=True)
+        # regression_funcs.compute_regression(subject,['Intercept','surprise_100','Surprisenp1','RepeatAlter','RepeatAlternp1'],"",filter_name,remap_grads=True)
+        regression_funcs.compute_regression(subject,['Complexity','WithinChunkPosition','RepeatAlter','ChunkBeginning', 'ChunkEnd', 'ChunkNumber', 'ChunkDepth'],"/Intercept_surprise_100_Surprisenp1_RepeatAlter_RepeatAlternp1/"+subject+"/residuals--remapped_clean-epo.fif",filter_name,remap_grads=True)
 
 
 # ----------------------------------------------------------------------------------------------------------------------
