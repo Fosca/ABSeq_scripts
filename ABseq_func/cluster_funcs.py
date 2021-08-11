25aed809bc63b42abe6b10bfcb2a7db8af908876
Fosca <fosca.alroumi@gmail.com>
2021-08-11 13:54:02 +0200

    linear reg of structure on residuals

--- ABseq_func/cluster_funcs.py
+++ ABseq_func/cluster_funcs.py
@@ -275,13 +275,26 @@ def ord_code_16items(subject,load_residuals_regression=False):
 #                            LINEAR REGRESSIONS
 # ----------------------------------------------------------------------------------------------------------------------
 
+
+
+
 def linear_reg(subject):
     from ABseq_func import regression_funcs
     filter_names = ['Hab', 'Stand', 'Viol']
     # regression_funcs.update_metadata_epochs_and_save_epochs(subject)
     for filter_name in filter_names:
-        regression_funcs.compute_regression(subject,['Intercept','surprise_100','Surprisenp1','RepeatAlter','RepeatAlternp1'],"",filter_name,remap_grads=True)
-        regression_funcs.compute_regression(subject,['Complexity','SequenceID','WithinChunkPosition','RepeatAlter','ChunkBeginning', 'ChunkEnd', 'ChunkNumber', 'ChunkDepth'],"/Intercept_surprise_100_Surprisenp1_RepeatAlter_RepeatAlternp1/"+subject+"/residuals--remapped_baselined_clean-epo.fif",filter_name,remap_grads=True)
+        # regressors_names = ['Complexity','SequenceID','WithinChunkPosition','RepeatAlter','ChunkBeginning', 'ChunkEnd', 'ChunkNumber', 'ChunkDepth']
+        # epochs_fname =
+        # remap_grads=True
+        # filter = ''
+        # subject = config.subjects_list[2]
+        # save_evoked_for_regressor_level=True
+        # cleaned=True
+        # apply_baseline=False
+        # suffix = ''
+        # filter_name = 'Hab'
+        # regression_funcs.compute_regression(subject,['Intercept','surprise_100','Surprisenp1','RepeatAlter','RepeatAlternp1'],"",filter_name,remap_grads=True)
+        regression_funcs.compute_regression(subject,['Complexity','SequenceID','WithinChunkPosition','RepeatAlter','ChunkBeginning', 'ChunkEnd', 'ChunkNumber', 'ChunkDepth'],"/Intercept_surprise_100_Surprisenp1_RepeatAlter_RepeatAlternp1/"+subject+"/residuals--remapped_clean-epo.fif",filter_name,remap_grads=True)
 
 
 # ----------------------------------------------------------------------------------------------------------------------
