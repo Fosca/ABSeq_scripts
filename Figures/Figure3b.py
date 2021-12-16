ec71d626ce6dc2105f77ea0e8fcdeceb649b6631
sam-planton <samuel.planton@gmail.com>
2021-12-16 13:13:11 +0100

    Figure 3b: linear regression

---
+++ Figures/Figure3b.py
@@ -0,0 +1,16 @@
+from ABseq_func import regression_funcs
+import config
+
+
+# =========================================================== #
+# Linear regression group analysis (2nd level)
+# =========================================================== #
+import matplotlib.pyplot as plt  # avoids the script getting stuck when plotting sources ?!
+
+filter_names = ['Hab', 'Stand', 'Viol']
+for filter_name in filter_names:
+
+    # Regression of complexity on original data - group analysis
+    regressors_names = ['Intercept', 'Complexity']
+    regression_funcs.merge_individual_regression_results(regressors_names, "", filter_name, suffix='--remapped_gtmbaselined')
+    regression_funcs.regression_group_analysis(regressors_names, "", filter_name, suffix='--remapped_gtmbaselined', Do3Dplot=False)
