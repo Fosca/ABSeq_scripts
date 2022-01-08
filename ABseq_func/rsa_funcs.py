9a50f34044b2809b60e23d328637345375aa4100
Fosca <fosca.alroumi@gmail.com>
2022-01-08 18:29:36 +0100

    added the MRI dissimilarity maps for language and maths network

--- ABseq_func/rsa_funcs.py
+++ ABseq_func/rsa_funcs.py
@@ -302,7 +302,7 @@ class dissimilarity:
     def Language_network(stim1, stim2):
         path_lang_network = config.scripts_path+'/RSA_predictors_from_MRI/PallierLang_avg_correlation_DSM.csv'
         lang_df = pd.read_csv(path_lang_network,header=None)
-        dissim_mat = lang_df.values.shape
+        dissim_mat = lang_df.values
         seq1 = stim1['SequenceID']
         seq2 = stim2['SequenceID']
 
@@ -313,7 +313,7 @@ class dissimilarity:
     def Maths_network(stim1, stim2):
         path_lang_network = config.scripts_path + '/RSA_predictors_from_MRI/MathsNetwork_avg_correlation_DSM.csv'
         math_df = pd.read_csv(path_lang_network, header=None)
-        dissim_mat = math_df.values.shape
+        dissim_mat = math_df.values
 
         seq1 = stim1['SequenceID']
         seq2 = stim2['SequenceID']
